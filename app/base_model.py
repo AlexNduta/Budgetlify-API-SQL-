09592a8ad3822723f256b735ba3279fdc83fa57d
was <was@localhost.localdomain>
2024-05-27 20:06:26 +0300

    tiny

--- app/base_model.py
+++ app/base_model.py
@@ -6,8 +6,8 @@ from pydantic import BaseModel, ValidationError
 from random import randrange
 from typing import List
 import uuid
-import psycopg2
-from psycopg2.extras import RealDictCursor
+import psycopg2 # for working with postgresql
+from psycopg2.extras import RealDictCursor # for giving columnname and value
 
 app = FastAPI()
 
@@ -16,13 +16,21 @@ filename = 'storage.txt'
 # as long as we are not able to connect to the DB, loop endlesly
 while True:
     try:
-        conn = psycopg2.connect(host='localhost', database='Budgelify', user='was', password='123', cursor_factory=RealDictCursor)
+        # connecting to the Database using psycopg2
+        # pass requirements
+        conn = psycopg2.connect(host='localhost',
+                                database='Budgelify', 
+                                user='was', 
+                                password='123', 
+                                cursor_factory=RealDictCursor)
+        # We use the cursor to execute SQL commands
+        # example: cursor.Execute('SQL commands go here')
         cursor= conn.cursor()
         print("succesfully connected to the DB")
         break # break from the loop when we are able to connect to the DB
     except Exception as error:
         print("Unable to connect to the db")
-        print('Error !!:', error)
+        print('Error:', error)
 
 expense_array = []
 
@@ -32,7 +40,7 @@ def expense_file_reader(filename) -> List[dict]:
         with open(filename, "r") as f:
             data = json.load(f)
         if not isinstance (data, (list, dict)):
-            raise ValueError("Inavalid JSON format. Top-level list must be either a list or an object")
+            raise ValueError("Invalid JSON format. Top-level list must be either a list or an object")
         if isinstance(data, dict):
             expenses = data.get("expenses", [])
         else:
@@ -61,9 +69,10 @@ def expense_file_writer(expenses: List[dict]):
 def get_expenses():
     """ get a list of all our expenses saved someehere in the file/DB"""
    # expenses = expense_file_reader(filename)
+   # execute SQL statements in our code
     cursor.execute("""SELECT * FROM Expenses""")
-    expenses = cursor.fetchall()
-    return expenses
+    expenses = cursor.fetchall() # get all items from the database
+    return expenses 
 
 
 class Post(BaseModel):
@@ -85,9 +94,10 @@ def post_expense(new_expense: Post):
     """
 
     try:
+        # use cursor object to execute SQL statements   
         cursor.execute("""INSERT INTO expenses(category, amount, Description) VALUES(%s, %s, %s) RETURNING *""", (new_expense.category, new_expense.amount, new_expense.description))
-        new_expense_posted = cursor.fetchone()
-        conn.commit()
+        new_expense_posted = cursor.fetchone() # get the values posted in the SQL statement above
+        conn.commit() # save the data to the database
         return {"expense posted": new_expense_posted}
     except Exception as e:
         raise HTTPException(status_code=500, detail="An error occured : {}".format(e))
@@ -97,10 +107,11 @@ def post_expense(new_expense: Post):
 
 @app.get("/Budgetlify/expenses/{id}")
 def get_single_item(id:int):
-    """ gets a single item from the list of expenses with the specified ID"""
-    cursor.execute("""SELECT * FROM expenses WHERE id = %s""", (str(id)))
-    fetched_item = cursor.fetchone()
-    
+    """ gets a single item from the list of expenses
+        with the specified ID"""
+    cursor.execute("""SELECT * FROM expenses WHERE id = %s""", (str(id),)) # make sure to covert the passed ID to astring
+    fetched_item = cursor.fetchone() # get that one item from the list
+    # confirm if we were able to get the item else, raise an error
     if fetched_item:
         return fetched_item
     else:
@@ -121,12 +132,14 @@ def delete_a_single_item(id:int):
     """
     deletes an item specified by ID from the list
     """
-    cursor.execute("""DELETE FROM expenses WHERE id = %s returning *""", (str(id)))
-    deleted_post = cursor.fetchone()
-    conn.commit()
+    # use cursor to execute SQL statements
+    cursor.execute("""DELETE FROM expenses WHERE id = %s returning *""", (str(id),))
+    deleted_post = cursor.fetchone() # delete the specified item
+    conn.commit() # make changes to the database
+
+    # confirm if the item exists
     if deleted_post is None:
-        return {"message":"Item deleted successfully"}
-        raise HTTPException(status_code=404, detail="Expense not found")
+        raise HTTPException(status_code=404, detail="Expense with ID {} not found".format(id))
 
     return {"message":"Item deleted successfully"}
 
@@ -134,6 +147,8 @@ def delete_a_single_item(id:int):
 def update_expense(id:int, post:Post):
     """ updates existing items with user changes in the list maintaining the id"""
 
+
+    #  use the cursor to execute the SQL statements
     cursor.execute("""UPDATE expenses SET category=%s, amount=%s, description=%s WHERE id=%s returning *""", (post.category, post.amount, post.description, str(id)))
     updated_expense = cursor.fetchone()
     conn.commit()
